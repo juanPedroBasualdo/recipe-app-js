dc9889187b96c391c65d0be54f19533f13a0af09
juanPedroBasualdo <jpbasualdo@fi.uba.ar>
2026-02-14 16:45:07 -0300

    Conectado app con spoonacular api y evito usar muchos creditos diarios con isFirstRender

--- recipe-app/src/components/Search.jsx
+++ recipe-app/src/components/Search.jsx
@@ -1,22 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "e7887d6f4f974b64a9dde6370c224e58";
 
 export default function Search() {
   const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
+  const isFirstRender = useRef(true);
+
   useEffect(() => {
-    function demo() {
-      console.log("Demo");
+    async function fetchRecipe() {
+      const response = await fetch(`${URL}?query=${search}&apiKey=${API_KEY}`);
+      const data = response.json();
+      console.log(data.results);
     }
-    demo();
-  }, [query]);
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    fetchRecipe();
+  }, [search]);
+
+  function handleSearch(e) {
+    e.preventDefault();
+    setSearch(query);
+  }
 
   return (
     <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Buscar una receta..."
-      />
+      <form onSubmit={(e) => handleSearch(e)}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Buscar una receta..."
+        />
+        <button type="submit">Buscar</button>
+      </form>
     </div>
   );
 }
