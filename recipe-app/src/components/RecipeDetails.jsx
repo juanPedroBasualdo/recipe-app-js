406d0e78488db56608d16d477d4642040aa02af6
juanPedroBasualdo <jpbasualdo@fi.uba.ar>
2026-02-20 22:25:12 -0300

    Añado los detalles de las recetas como componente terminado

--- recipe-app/src/components/RecipeDetails.jsx
+++ recipe-app/src/components/RecipeDetails.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import styles from "./recipedetails.module.css";
 
 export default function RecipeDetails({ detailsId }) {
   const URL = `https://api.spoonacular.com/recipes/${detailsId}/information`;
   const API_KEY = "e7887d6f4f974b64a9dde6370c224e58";
 
+  const [isLoading, setLoading] = useState(true);
   const [details, setDetails] = useState({});
 
   useEffect(() => {
@@ -12,6 +14,7 @@ export default function RecipeDetails({ detailsId }) {
       const data = await response.json();
       console.log(data);
       setDetails(data);
+      setLoading(false);
     }
 
     if (detailsId !== "") {
@@ -19,11 +22,39 @@ export default function RecipeDetails({ detailsId }) {
     }
   }, [detailsId]);
 
-  return (
-    <div>
-      <h1>{details.title}</h1>
-      <img src={details.image} alt="Recipe Image" />
-      {details.instructions}
-    </div>
-  );
+  if (detailsId !== "") {
+    return (
+      <div>
+        <div className={styles.recipecard}>
+          <h1>{details.title}</h1>
+          <img src={details.image} alt="Recipe Image" />
+        </div>
+        <span>
+          <strong>⏲️ {details.readyInMinutes} minutes </strong>
+        </span>
+        <span>
+          {details.vegetarian ? "🥬 Vegetarian" : "🥩 Non-Vegetarian"}
+        </span>
+        <span>{details.vegan ? "🍄‍🟫 Vegan" : "🥚 Non-Vegan"}</span>
+        <span>
+          <strong>🍴 Servings: {details.servings} people</strong>
+        </span>
+
+        <div className={styles.recipeinstructions}>
+          <h2>Instructions</h2>
+          <ol>
+            {isLoading ? (
+              <p> Loading.. </p>
+            ) : (
+              details.analyzedInstructions[0].steps.map((step) => (
+                <li key={step.number}>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+
+  return <div></div>;
 }
